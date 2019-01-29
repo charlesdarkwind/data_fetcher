815c06488fe4e0406bcedf7d32b0e3a721c66fa5
charlesdarkwind <guildwarsguy12@gmail.com>
2019-01-29 15:31:20 -0500

    docs, removing and refactoring for new use cases

--- appController.js
+++ appController.js
@@ -1,14 +1,21 @@
-const { binance, updateExchangeInfos } = require('./ressources');
-
+/**
+ * data_fetcher
+ * @module charlesdarkwind/data_fetcher
+ * @return {object} instance to class object
+ */
 const depthManager = async () => {
     let depthManager = this;
-    'use strict'
+    'use strict';
 
-    // Update exchange infos json file
-    await updateExchangeInfos(); // todo make return the obj AND write the file
+    const { binance, getExchangeInfos, getBtcPairs, getPairs } = require('./mod_pairs');
+    const excludedPairs = require('./params.json').excludedPairs;
+    depthManager.exchangeInfos = await getExchangeInfos();
+    const allPairsBTC = getBtcPairs(depthManager.exchangeInfos);
+
+    const pairs = getPairs(allPairsBTC, excludedPairs);
 
     const startDepth = () => {
 
     };
-
 };
+module.exports = depthManager();
---
+++ mod_pairs.js
@@ -0,0 +1,57 @@
+const rp = require('request-promise');
+
+const requestOptions = {
+    method: 'GET',
+    uri: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
+    qs: {
+        start: 1,
+        limit: 150,
+        convert: 'BTC',
+        sort: 'market_cap'
+    },
+    headers: {'X-CMC_PRO_API_KEY': process.env.CMC},
+    json: true,
+    gzip: true
+};
+
+
+/**
+ * @returns {Promise<object>} - General infos of the exchange and the pairs
+ */
+exports.getExchangeInfos = () => new Promise((resolve, reject) => {
+    binance.exchangeInfo((err, data) => err ? reject(err) : resolve(data));
+});
+
+
+/**
+ * Get all BTC pairs of binance
+ * @param exchangeInfos - Object obtained after GETting the endpoint with same name on Binance'e API
+ * @returns {array} - All BTC pairs of binance
+ */
+exports.getBtcPairs = exchangeInfos => exchangeInfos.symbols.filter(
+    pair => pair.quoteAsset === 'BTC'
+        && pair.status === 'TRADING'
+        && !excludedPairs.includes(pair))
+    .map(pair => pair.symbol);
+
+
+/**
+ * Find pairs with highest market cap with regards to both a list to include and a list to exclude.
+ * @param {array} pairsToInclude - Array of pairs of interest to work with
+ * @param {array} excludedPairs - Array of pairs to exclude
+ * @return {Promise<array>} result - Array of pairs with highest market cap
+ */
+exports.getPairs = (pairsToInclude, excludedPairs) => new Promise((resolve, reject) => {
+    const result = [];
+    rp(requestOptions).then(res => {
+        res.data.map(body => {
+            const pairName = `${body.symbol}BTC`;
+            if (pairsToInclude.includes(pairName)
+                && !excludedPairs.includes(pairName)
+                && body.quote["BTC"].price > 0.00000200) {
+                result.push(pairName);
+            }
+        });
+        resolve(result);
+    }).catch(err => reject(err));
+});
