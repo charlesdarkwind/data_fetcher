22ecdc40c98793328c72d90112ae648bdac0cca3
Jasmin Parent <guildwarsguy12@gmail.com>
2019-01-29 07:52:19 -0500

    initial

---
+++ start.js
@@ -0,0 +1,7 @@
+const appController =  require('appController')
+
+process.on('uncaughtException', err => {
+    console.log(err);
+});
+
+process.on('unhandledRejection', (reason, p) => console.warn('Unhandled Rejection at: Promise', p, 'reason:', reason));
