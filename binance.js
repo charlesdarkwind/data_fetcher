b36d0c50248500924c170f5f2a3fe4e6f2fe546e
charlesdarkwind <guildwarsguy12@gmail.com>
2019-01-30 14:00:34 -0500

    apparently was not tracked

---
+++ binance.js
@@ -0,0 +1,18 @@
+const Binance = require('node-binance-api');
+const moment = require('moment');
+
+const binance = new Binance().options({
+    test: false,
+    APIKEY: process.env.APIKEY,
+    APISECRET: process.env.APISECRET,
+    useServerTime: true,
+    recvWindow: 20000,
+    verbose: true,
+    log(...args) {
+        console.warn(
+            Array.prototype.slice.call(args),
+            moment().format('MMM D, H:mm:ss')
+        );
+    }
+});
+module.exports = binance;
---
+++ mod_depth.js
@@ -0,0 +1,66 @@
+/**
+ * data_fetcher
+ * @module charlesdarkwind/data_fetcher
+ * @return {object} instance to class object
+ */
+let DepthManager = function () {
+    'use strict';
+
+    const fs = require('fs');
+    const zlib = require('zlib');
+    const {getExchangeInfos, getBtcPairs, getPairs} = require('./mod_pairs');
+    const binance = require('./binance');
+    const params = require('./params.json');
+    const depthMain = {};
+    let exchangeInfos = {};
+    let allPairsBTC = [];
+    let pairs = [];
+    let options = {
+        saveInterval: 10000,
+        pairsNum: 100
+    };
+
+
+    /**
+     * Start periodically saving depth of all pairs to a new file at each intervals.
+     * @returns {undefined}
+     */
+    const savePeriodically = () => {
+        setInterval(() => {
+            if (!fs.existsSync('./depths')) fs.mkdirSync('./depths'); // Create folder
+
+            const gzip = zlib.createGzip({level: 9});
+            const name = `./depths/depth_${Date.now()}.json.gz`;
+            const data = JSON.stringify(depthMain, null, 0);
+            const out = fs.createWriteStream(name);
+
+            gzip.pipe(out);
+            gzip.write(data, (err) => {
+                if (err) throw err;
+                gzip.end();
+            });
+        }, options.saveInterval);
+    };
+
+    return {
+
+
+        init: async function () {
+            exchangeInfos = await getExchangeInfos();
+            allPairsBTC = getBtcPairs(exchangeInfos, params.excludedPairs);
+            pairs = await getPairs(allPairsBTC, params.excludedPairs);
+            pairs = pairs.slice(0, options.pairsNum);
+        },
+
+
+        startWS: function () {
+            binance.websockets.depthCache(pairs, (symbol, depth) => depthMain[symbol] = depth);
+        },
+
+
+        startSaving: function () {
+            savePeriodically();
+        }
+    };
+};
+module.exports = DepthManager;
